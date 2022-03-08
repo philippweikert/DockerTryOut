c92afe7c22894252aa180a8a85d8d5277ee8fce6
philipp.weikert <philippweikert3@gmail.com>
2022-03-08 11:26:49 +0100

    first commit

---
+++ src/main/java/de/neuefische/tryitout/dockertryout/DockerTryOutApplication.java
@@ -0,0 +1,13 @@
+package de.neuefische.tryitout.dockertryout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerTryOutApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerTryOutApplication.class, args);
+    }
+
+}
---
+++ src/main/java/de/neuefische/tryitout/dockertryout/TryOutController.java
@@ -0,0 +1,16 @@
+package de.neuefische.tryitout.dockertryout;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/greetings")
+public class TryOutController {
+
+    @GetMapping ("/{name}")
+    public String greetPeople (@PathVariable String name){
+        return "Moinsen " + name;
+    }
+}
